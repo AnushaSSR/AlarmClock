9cfb506d705cc839c24805b8013b8f407b6774ba
Anusha <nssranusha@yahoo.co.in>
2022-08-28 23:04:29 +0530

    Main.js code : implemented code to create the list of alarms

--- main.js
+++ main.js
@@ -28,3 +28,60 @@ function setDigit(timeVal) {
     if ((timeVal >= 10) || (timeVal.toString().length == 2)) { return timeVal; }
     else { return "0" + timeVal; }
 }
+
+
+//set the alarm, adds alarm to the list 
+let alarmsList = [];
+function setAlarm() {
+    // console.log("asjfhjfk");
+
+    let hourVal = document.getElementById('hour-val');
+    let minVal = document.getElementById('min-val');
+    let secVal = document.getElementById('sec-val');
+    let ampmVal = document.getElementById('am-pm');
+
+    let alarmSetValue = `${setDigit(hourVal.value)}:${setDigit(minVal.value)}:${setDigit(secVal.value)} ${ampmVal.value}`;
+
+
+
+    if (alarmsList.indexOf(alarmSetValue) > -1) {
+        alert("Alarm already set for the specified time");
+
+    }
+    else {
+        alarmsList.push(alarmSetValue);
+        // sortList(alarmsList);
+        populateAlarmList(alarmsList);
+    }
+
+}
+
+// to populate the list with the added alarms by the user
+function populateAlarmList(alarmsList) {
+
+    let list = document.getElementById('alarms-list');
+    list.innerHTML = "";
+
+    for (index = 0; index < alarmsList.length; index++) {
+        const element = alarmsList[index];
+        const indexVal = index;
+        let listElement = document.createElement('li');
+        let deleteButton = document.createElement('button');
+        let alarmElement = document.createElement('span');
+        deleteButton.classList.add('deleteListElement');
+        alarmElement.innerHTML = element;
+        deleteButton.innerText = "Delete";
+
+        // deleteButton.addEventListener('click', () => {
+        //     deleteListElement(indexVal);
+        // });
+
+        listElement.appendChild(alarmElement);
+        listElement.appendChild(deleteButton);
+
+
+        list.appendChild(listElement);
+
+    }
+
+}
